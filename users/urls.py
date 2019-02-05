9b142831ae8488598cb45976b5d33064aa124b03
pumuckelo <abdou.seck.business@gmail.com>
2019-02-05 17:01:32 +0100

    commit 5

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,7 @@ from . import views
 #Url redirection to use the views for specific paths
 urlpatterns = [
     path('', views.neworder, name='users-home'),
+    path('history/', views.orderhistory, name='orderhistory'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact')
 ]
--- users/views.py
+++ users/views.py
@@ -5,8 +5,10 @@ from django.contrib import messages
 
 # Create your views here.
 
-#def home(request):
- #   return render(request, 'users/home.html')
+# def home(request):
+#   return render(request, 'users/home.html')
+
+# Function to render home.html with the order form to make new orders
 def neworder(request):
     if request.method == 'POST':
         form = orderForm(request.POST)
@@ -19,3 +21,16 @@ def neworder(request):
         'orders': order.objects.all()
     }
     return render(request, 'users/home.html', {'form': form}, context)
+
+# Function to render the orderhistory.hthml, which will show all orders filtered
+# by username
+def orderhistory(request):
+    return render(request, 'users/orderhistory.html')
+
+# Function to render about.html
+def about(request):
+    return render(request, 'users/about.html')
+
+# Function to render contact.html
+def contact(request):
+    return render(request, 'users/contact.html')
