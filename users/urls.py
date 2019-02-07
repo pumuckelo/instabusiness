63b3c7254ca9a554e7084582f8384669276c6059
pumuckelo <abdou.seck.business@gmail.com>
2019-02-07 16:53:16 +0100

    commit 10, added crispy forms and login/logout view

--- users/urls.py
+++ users/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
     path('register/', views.register, name='register'),
-    path('login', auth_views.LoginView.as_view(), name='login'),
-    path('logout', auth_views.LogoutView.as_view(), name='logout')
+    path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
