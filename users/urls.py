2d4ca321a49b06e733dcc5514187c745ff1718e1
pumuckelo <abdou.seck.business@gmail.com>
2019-02-07 15:58:10 +0100

    commit 8, register form and added homepage(index.html), changed the navbar a bit and  added some general styling

--- users/urls.py
+++ users/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 #Url redirection to use the views for specific paths
 urlpatterns = [
-    path('', views.neworder, name='users-home'),
+    path('', views.index, name='index'),
+    path('order/', views.neworder, name='order'),
     path('history/', views.orderhistory, name='orderhistory'),
     path('about/', views.about, name='about'),
-    path('contact/', views.contact, name='contact')
+    path('contact/', views.contact, name='contact'),
+    path('register/', views.register, name='register'),
+    path('login', views.login, name='login'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import orderForm
 from .models import order
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+#Function for index.html
+def index(request):
+   return render(request, 'users/index.html')
 
-# def home(request):
-#   return render(request, 'users/home.html')
-
-# Function to render home.html with the order form to make new orders
+# Function to render order.html with the order form to make new orders
 def neworder(request):
     if request.method == 'POST':
         form = orderForm(request.POST)
@@ -18,10 +19,27 @@ def neworder(request):
     else:
         form = orderForm()
 
-    return render(request, 'users/home.html', {'form': form})
+    return render(request, 'users/order.html', {'form': form})
 
-# Function to render the orderhistory.hthml, which will show all orders filtered
-# by username
+
+#Function for Register
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Dein Account {username} wurde erstellt')
+            return redirect ('order')
+            form.save()
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
+
+#Function for Login
+def login(request):
+    return render(request,  'users/login.html')
+
+# Function to render the orderhistory.hthml, which will show all orders filtered by username
 def orderhistory(request):
     context = {
         'orders': order.objects.all()
